9dd245338b8de0f61baea2a47575e555a419ab4d
Simon Jung <vicevil4@gmail.com>
2022-11-13 11:34:14 +0900

    4 add health checker (#5)
    * add Health Status Controller
    * add logback
    * add git commit information

---
+++ src/main/java/io/vicevil4/slogging/api/module/controller/HealthController.java
@@ -0,0 +1,35 @@
+package io.vicevil4.slogging.api.module.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.vicevil4.slogging.api.module.dto.HealthDto;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/health")
+@Slf4j
+public class HealthController {
+
+  @Value("${git.branch}")
+  private String gitBranch;
+
+  @Value("${git.build.version}")
+  private String gitBuildVersion;
+
+  @Value("${git.commit.id.full}")
+  private String gitCommitId;
+  
+  @RequestMapping(value = "/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<HealthDto> healthStatus() {
+    HealthDto result = new HealthDto(LocalDateTime.now(), gitBranch, gitBuildVersion, gitCommitId);
+    log.debug("result : {}", result);
+    return ResponseEntity.ok(result);
+  }
+}
---
+++ src/main/java/io/vicevil4/slogging/api/module/dto/HealthDto.java
@@ -0,0 +1,22 @@
+package io.vicevil4.slogging.api.module.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+@RequiredArgsConstructor
+@Getter 
+@ToString(includeFieldNames = true)
+public class HealthDto {
+
+  private @NonNull LocalDateTime serverTime;
+
+  private @NonNull String gitBranch;
+  
+  private @NonNull String gitBuildVersion;
+
+  private @NonNull String gitCommitId;
+}
