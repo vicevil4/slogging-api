79c668eafbf222dddd462f972fe878a66d07e195
Simon Jung <vicevil4@gmail.com>
2022-11-20 22:29:22 +0900

    Update BoardController.java

--- src/main/java/io/vicevil4/slogging/api/module/controller/BoardController.java
+++ src/main/java/io/vicevil4/slogging/api/module/controller/BoardController.java
@@ -1,6 +1,7 @@
 package io.vicevil4.slogging.api.module.controller;
 
-import io.vicevil4.slogging.api.module.dto.HealthDto;
+import io.vicevil4.slogging.api.module.dto.BoardRequestDto;
+import io.vicevil4.slogging.api.module.dto.BoardResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
@@ -16,7 +17,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
 
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HealthDto> getBoardList() {
+    public ResponseEntity<BoardResponseDto> getBoardList() {
+        return ResponseEntity.ok(null);
+    }
+
+    @RequestMapping(value = "/{boardId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BoardResponseDto> createBoard(BoardRequestDto boardDto) {
+        return ResponseEntity.ok(null);
+    }
+
+    @RequestMapping(value = "/{boardId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BoardResponseDto> updateBoard(BoardRequestDto boardDto) {
+        return ResponseEntity.ok(null);
+    }
+
+    @RequestMapping(value = "/{boardId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deleteBoard(long boardId) {
         return ResponseEntity.ok(null);
     }
 }
