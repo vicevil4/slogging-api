0b5ebd19d62ca9bb7a9d6972ad73caa68dd1624e
Simon Jung <vicevil4@gmail.com>
2022-12-04 23:59:23 +0900

    modify logging filter for form/json

--- src/main/java/io/vicevil4/slogging/api/filter/LoggingFilter.java
+++ src/main/java/io/vicevil4/slogging/api/filter/LoggingFilter.java
@@ -5,6 +5,7 @@ import io.vicevil4.slogging.api.filter.internal.ResponseWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.InvalidMediaTypeException;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
@@ -15,6 +16,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.List;
@@ -43,37 +45,77 @@ public class LoggingFilter extends OncePerRequestFilter {
         RequestWrapper requestWrapper = new RequestWrapper(request);
         ResponseWrapper responseWrapper = new ResponseWrapper(response);
 
-        loggingRequest(requestWrapper);
+        MediaType requestContentType = getRequestContentType(requestWrapper);
+
+        final String requestInfo = getRequestInfo(requestWrapper);
+
+        // 요청정보의 컨텐츠 타입이 FORM 타입인 경우 캐싱이 가능하므로 전체요청정보를 선행에서 로깅한다.
+        if (MediaType.APPLICATION_FORM_URLENCODED.includes(requestContentType)) {
+            log.info("Request {} Payload={}", requestInfo, getRequestFormParam(requestWrapper));
+        }
 
         filterChain.doFilter(requestWrapper, responseWrapper);
 
+        // FORM 타입이 아닌 경우는 전체요청정보를 후행에서 로깅한다.
+        if (!MediaType.APPLICATION_FORM_URLENCODED.includes(requestContentType)) {
+            log.info("Request {} Payload={}", requestInfo, getRequestBodyParam(requestWrapper));
+        }
         loggingResponse(responseWrapper);
         responseWrapper.copyBodyToResponse();
 
         MDC.clear();
     }
 
-    private void loggingRequest(RequestWrapper requestWrapper) {
-
-        String queryString = requestWrapper.getQueryString();
-        String headerString = getHeaders(requestWrapper);
-        log.info("Request {} {}{} {} Header: {}",
-                requestWrapper.getMethod()
-                , requestWrapper.getRequestURI()
-                , null != queryString ? "?" + queryString : ""
-                , requestWrapper.getProtocol()
-                , headerString
-        );
-
-        if (isLoggingAvailable(requestWrapper.getContentType())) {
-            byte[] content = requestWrapper.getContentAsByteArray();
-            if (content.length > 0) {
-                String contentString = new String(content);
-                log.info("Request Payload={}", contentString);
-            }
-        } else {
-            log.debug("Request Payload is Binary Content.");
+    private MediaType getRequestContentType(RequestWrapper requestWrapper) {
+        if (null == requestWrapper.getContentType()) return MediaType.APPLICATION_FORM_URLENCODED;
+        try {
+            MediaType mediaType = MediaType.valueOf(requestWrapper.getContentType());
+            return mediaType;
+        } catch (InvalidMediaTypeException e) {
+            log.debug("Request ContentType is invalid {}", e.getMessage());
+            return MediaType.APPLICATION_FORM_URLENCODED;
         }
+    }
+
+    private String getRequestInfo(RequestWrapper requestWrapper) {
+
+        final StringBuilder sb = new StringBuilder();
+        final String queryString = requestWrapper.getQueryString();
+        String headerString = getHeaders(requestWrapper);
+        sb.append(requestWrapper.getMethod())
+                .append(" ")
+                .append(requestWrapper.getRequestURI())
+                .append(null != queryString ? "?" + queryString : "")
+                .append(" ")
+                .append(requestWrapper.getProtocol())
+                .append(" ")
+                .append(headerString);
+        return sb.toString();
+    }
+
+    private String getRequestFormParam(RequestWrapper requestWrapper) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        final Enumeration<?> params = requestWrapper.getParameterNames();
+        while (params.hasMoreElements()) {
+            final String name = (String) params.nextElement();
+            if (null != name) {
+                sb.append(name).append("=").append(requestWrapper.getParameter(name));
+            }
+            if (params.hasMoreElements()) {
+                sb.append(",");
+            }
+        }
+        return sb.toString();
+    }
+
+    private String getRequestBodyParam(RequestWrapper requestWrapper) throws IOException {
+
+        byte[] content = requestWrapper.getContentAsByteArray();
+        if (content.length > 0) {
+            String contentString = new String(content, StandardCharsets.UTF_8);
+            return contentString;
+        }
+        return "";
     }
 
     private String getHeaders(RequestWrapper requestWrapper) {
--- src/main/java/io/vicevil4/slogging/api/filter/internal/RequestWrapper.java
+++ src/main/java/io/vicevil4/slogging/api/filter/internal/RequestWrapper.java
@@ -3,7 +3,6 @@ package io.vicevil4.slogging.api.filter.internal;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 public class RequestWrapper extends ContentCachingRequestWrapper {
 
